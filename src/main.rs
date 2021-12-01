83482ea4f3930192444dbd95fe406cc07fa28e39
Mark Lister <mlister@scottlogic.com>
2021-12-01 11:46:09 +0000

    Split day 1 inputs out

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 
 
 fn main() {
-    day1();
+    day1_part1();
 }
 
-fn day1() {
-    let day_1_input = include_str!("inputs/day1.txt");
-    let input = day_1_input.split("\n").map(|s| s.trim());
-
-    let values = input.map(|s| s.parse().unwrap()).collect::<Vec<i32>>();
+fn day1_part1() {
+    let values = day1_input();
 
     let mut last_value : Option<i32> = None;
     let mut output = 0;
@@ -25,5 +22,14 @@ fn day1() {
         }
     }
 
-    println!("Day 1 output: {}", output);
+    println!("Day 1 part 1 output: {}", output);
+}
+
+fn day1_input() -> Vec<i32> {
+    let day_1_input = include_str!("inputs/day1.txt");
+    let input = day_1_input.split("\n").map(|s| s.trim());
+
+    let values = input.map(|s| s.parse().unwrap()).collect::<Vec<i32>>();
+
+    values
 }
