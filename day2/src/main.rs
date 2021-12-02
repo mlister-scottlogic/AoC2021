324da703d8d0b5fee7656aae126130205b6e19a3
Mark Lister <mlister@scottlogic.com>
2021-12-02 09:35:05 +0000

    Implement part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
     let input = get_input();
 
-    println!("Day 2 Part 2: {}", part1(&input));
+    println!("Day 2 Part 1: {}", part1(&input));
+    println!("Day 2 Part 2: {}", part2(&input));
 }
 
 fn part1(input: &Vec<ControlMessage>) -> i32 {
@@ -12,6 +13,24 @@ fn part1(input: &Vec<ControlMessage>) -> i32 {
             ControlType::Up => (x, y - control_message.value),
         }
     });
+
+    coordinates.0 * coordinates.1
+}
+
+fn part2(input: &Vec<ControlMessage>) -> i32 {
+    let coordinates =
+        input.iter().fold(
+            (0, 0, 0),
+            |(x, y, aim), control_message| match control_message.control_type {
+                ControlType::Forward => (
+                    x + control_message.value,
+                    y + control_message.value * aim,
+                    aim,
+                ),
+                ControlType::Down => (x, y, aim + control_message.value),
+                ControlType::Up => (x, y, aim - control_message.value),
+            },
+        );
 
     coordinates.0 * coordinates.1
 }
