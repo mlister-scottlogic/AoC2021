129aba7d927ff53f354c73c424a7986646a3f5e4
Mark Lister <mlister@scottlogic.com>
2021-12-03 09:45:55 +0000

    Refactor part 1 and add some of part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,45 +1,71 @@
 fn main() {
     let input = get_input();
 
-    let most_common = most_common(&input);
-
-    println!("Day 3 Part 1: {}", part1(&most_common));
+    println!("Day 3 Part 1: {}", part1(&input));
+    // println!("Day 3 Part 2: {}", part2(&input));
 }
 
-fn part1(inputs: &(String, String)) -> i32 {
-    let gamma = i32::from_str_radix(&inputs.0, 2).unwrap();
-    let epsilon = i32::from_str_radix(&inputs.1, 2).unwrap();
+fn part1(readings: &Vec<Vec<char>>) -> i32 {
+    let gamma_string = most_common(readings, '1').iter().collect::<String>();
+    let epsilon_string = most_common(readings, '0').iter().collect::<String>();
+
+    let gamma = i32::from_str_radix(&gamma_string, 2).unwrap();
+    let epsilon = i32::from_str_radix(&epsilon_string, 2).unwrap();
 
     gamma * epsilon
 }
 
-fn most_common(input: &Vec<Vec<char>>) -> (String, String) {
+// fn part2(readings: &Vec<Vec<char>>, most_common: &(String, String)) -> i32 {
+//     let oxygen_generator = hidden_reading_extractor(readings, &most_common.0.chars().collect(), 0);
+//     let co2_scrubber = hidden_reading_extractor(readings, &most_common.1.chars().collect(), 0);
+
+//     println!("{}", oxygen_generator);
+//     println!("{}", co2_scrubber);
+
+//     oxygen_generator * co2_scrubber
+// }
+
+// fn hidden_reading_extractor(
+//     input: &Vec<Vec<char>>,
+//     index: usize,
+//     find_most_common: &dyn Fn(&Vec<Vec<char>>) -> Vec<char>,
+// ) -> i32 {
+//     if input.len() == 1 {
+//         let hidden_reading_string = input[0].iter().collect::<String>();
+
+//         return i32::from_str_radix(&hidden_reading_string, 2).unwrap();
+//     }
+
+//     let still_valid_inputs = input
+//         .iter()
+//         .filter(|c| c[index] == bit_matcher[index])
+//         .cloned()
+//         .collect();
+
+//     let new_index = index + 1;
+
+//     hidden_reading_extractor(&still_valid_inputs, bit_matcher, new_index)
+// }
+
+fn most_common(input: &Vec<Vec<char>>, target: char) -> Vec<char> {
     let input_length = input[0].len();
     let mut position_counter = vec![0; input_length];
 
     for binary_string in input {
-        for (i, c) in binary_string.iter().enumerate() {
-            match c {
-                '0' => position_counter[i] -= 1,
-                '1' => position_counter[i] += 1,
-                _ => panic!("Unknown binary value"),
+        for i in 0..binary_string.len() {
+            if binary_string[i] == target {
+                position_counter[i] += 1
+            } else {
+                position_counter[i] -= 1
             }
         }
     }
 
-    let gamma_string = position_counter
+    position_counter
         .iter()
         .copied()
-        .map(|v| if v >= 0 { "1" } else { "0" })
-        .collect::<String>();
-
-    let epsilon_string = position_counter
-        .iter()
-        .copied()
-        .map(|v| if v >= 0 { "0" } else { "1" })
-        .collect::<String>();
-
-    (gamma_string, epsilon_string)
+        .map(|v| if v >= 0 { '1' } else { '0' })
+        .collect()
 }
 
 fn get_input() -> Vec<Vec<char>> {
