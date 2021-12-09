71b29ec0f22358d139c208f1d18722a7950d752c
Mark Lister <mlister@scottlogic.com>
2021-12-09 09:48:47 +0000

    Add input parsing

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let input = get_input();
+    println!("{:?}", input);
+}
+
+fn get_input() -> Vec<Vec<u8>> {
+    let raw_input = include_str!("testinput.txt");
+    let input_lines = raw_input.lines();
+
+    input_lines
+        .map(|s| {
+            s.chars()
+                .map(|c| c.to_string().parse::<u8>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
 }
