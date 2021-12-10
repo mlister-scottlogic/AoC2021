d39f657dd56f96f892414ced5bdb7a987fcf78bc
Mark Lister <mlister@scottlogic.com>
2021-12-10 09:32:49 +0000

    Add part 1 implementation

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,86 @@
 fn main() {
-    println!("Hello, world!");
+    let input = get_input();
+
+    println!("Day 10 Part 1 {}", part1(&input));
+}
+
+fn part1(input: &Vec<Vec<char>>) -> u32 {
+    input
+        .iter()
+        .fold(0, |total, row| total + find_illegal_char_score(row))
+}
+
+fn find_illegal_char_score(row: &Vec<char>) -> u32 {
+    let mut opening_chars = vec![];
+
+    for c in row {
+        match c {
+            '(' => opening_chars.push('('),
+            '[' => opening_chars.push('['),
+            '{' => opening_chars.push('{'),
+            '<' => opening_chars.push('<'),
+
+            ')' => match opening_chars.last() {
+                Some(x) => {
+                    if *x != '(' {
+                        return 3;
+                    }
+
+                    opening_chars.pop();
+                }
+                None => {
+                    return 3;
+                }
+            },
+            ']' => match opening_chars.last() {
+                Some(x) => {
+                    if *x != '[' {
+                        return 57;
+                    }
+
+                    opening_chars.pop();
+                }
+                None => {
+                    return 57;
+                }
+            },
+            '}' => match opening_chars.last() {
+                Some(x) => {
+                    if *x != '{' {
+                        return 1197;
+                    }
+
+                    opening_chars.pop();
+                }
+                None => {
+                    return 1197;
+                }
+            },
+            '>' => match opening_chars.last() {
+                Some(x) => {
+                    if *x != '<' {
+                        return 25137;
+                    }
+
+                    opening_chars.pop();
+                }
+                None => {
+                    return 25137;
+                }
+            },
+
+            _ => panic!("Unexpected character {}", c),
+        }
+    }
+
+    0
+}
+
+fn get_input() -> Vec<Vec<char>> {
+    let input = include_str!("input.txt");
+
+    input
+        .lines()
+        .map(|i| i.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>()
 }
