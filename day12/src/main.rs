63e8aed13c6763d6c843fcb00cfcffd67b833496
Mark Lister <mlister@scottlogic.com>
2021-12-13 16:42:11 +0000

    Implement part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -3,12 +3,119 @@ use std::collections::HashMap;
 fn main() {
     let input = get_input();
 
-    println!("{:?}", input);
+    // println!("{:?}", input);
     println!("Day 12 part 1 {:?}", part1(&input.clone()));
+    println!("Day 12 part 2 {:?}", part2(&input.clone()));
+}
+
+fn part2(caves: &HashMap<Cave, Vec<Cave>>) -> u32 {
+    let start_cave = Cave::Start;
+
+    visit_cave2(start_cave, caves.clone(), HashMap::new(), vec![])
+}
+
+fn visit_cave2(
+    current_cave: Cave,
+    caves: HashMap<Cave, Vec<Cave>>,
+    visited_caves: HashMap<Cave, u8>,
+    previously_visited: Vec<Cave>,
+) -> u32 {
+    // We've managed to get to the end
+    if current_cave == Cave::End {
+        // println!("{:?}", previously_visited);
+        return 1;
+    }
+
+    // println!("Visiting cave {:?}", current_cave);
+    let small_visited_twice = visited_caves.iter().any(|(cave, times)| match cave {
+        Cave::Small(_) => {
+            // println!("Visited a small node twice {}", times);
+            return *times >= 2;
+        }
+        _ => return false,
+    });
+    let mut caves = caves.clone();
+
+    let mut visited_caves = visited_caves.clone();
+    let times_visited = visited_caves.entry(current_cave.clone()).or_insert(0);
+    *times_visited += 1;
+
+    let cannot_visit_small_cave_again = *times_visited >= 2 || small_visited_twice;
+
+    // Drop out if we're trying to visit a small cave twice, and we've already visited one
+    if *times_visited >= 2 && small_visited_twice {
+        match current_cave {
+            Cave::Small(_) => return 0,
+            _ => (),
+        }
+    }
+
+    // If small, remove from other caves possible routes
+    match current_cave {
+        Cave::Small(_) => {
+            for (_, c) in caves.iter_mut() {
+                if cannot_visit_small_cave_again {
+                    let remaining_values = c
+                        .iter()
+                        .filter(|ref x| ***x != current_cave)
+                        .map(|y| y.clone())
+                        .collect::<Vec<_>>();
+
+                    *c = remaining_values;
+                }
+            }
+        }
+        Cave::Start => {
+            for (_, c) in caves.iter_mut() {
+                let remaining_values = c
+                    .iter()
+                    .filter(|ref x| ***x != current_cave)
+                    .map(|y| y.clone())
+                    .collect::<Vec<_>>();
+
+                *c = remaining_values;
+            }
+        }
+        _ => (),
+    }
+
+    // println!("{:?}", caves);
+    let temp_cave = caves.clone();
+    let connected_caves = temp_cave
+        .get(&current_cave)
+        .expect("Unexpected missing node");
+
+    let mut newly_visited_caves = previously_visited.clone();
+    newly_visited_caves.push(current_cave.clone());
+
+    match current_cave {
+        Cave::Small(_) => {
+            if cannot_visit_small_cave_again {
+                caves.remove(&current_cave);
+            }
+        }
+        Cave::Start => {
+            caves.remove(&current_cave);
+        }
+        _ => (),
+    }
+
+    let mut total_routes = 0;
+    // For each cave connected visit
+    for connected_cave in connected_caves.iter() {
+        total_routes += visit_cave2(
+            connected_cave.clone(),
+            caves.clone(),
+            visited_caves.clone(),
+            newly_visited_caves.clone(),
+        )
+    }
+
+    total_routes
 }
 
 fn part1(caves: &HashMap<Cave, Vec<Cave>>) -> u32 {
-    let start_cave = Cave::Small("start".to_string());
+    let start_cave = Cave::Start;
 
     visit_cave(start_cave, caves.clone(), vec![])
 }
@@ -19,8 +126,8 @@ fn visit_cave(
     visited_caves: Vec<Cave>,
 ) -> u32 {
     // We've managed to get to the end
-    if current_cave == Cave::Small("end".to_string()) {
-        println!("{:?}", visited_caves);
+    if current_cave == Cave::End {
+        // println!("{:?}", visited_caves);
         return 1;
     }
 
@@ -31,6 +138,17 @@ fn visit_cave(
     // If small, remove from other caves possible routes
     match current_cave {
         Cave::Small(_) => {
+            for (_, c) in caves.iter_mut() {
+                let remaining_values = c
+                    .iter()
+                    .filter(|ref x| ***x != current_cave)
+                    .map(|y| y.clone())
+                    .collect::<Vec<_>>();
+
+                *c = remaining_values;
+            }
+        }
+        Cave::Start => {
             for (_, c) in caves.iter_mut() {
                 let remaining_values = c
                     .iter()
@@ -56,6 +174,9 @@ fn visit_cave(
         Cave::Small(_) => {
             caves.remove(&current_cave);
         }
+        Cave::Start => {
+            caves.remove(&current_cave);
+        }
         _ => (),
     }
 
@@ -76,6 +197,8 @@ fn visit_cave(
 enum Cave {
     Large(String),
     Small(String),
+    Start,
+    End,
 }
 
 fn get_input() -> HashMap<Cave, Vec<Cave>> {
@@ -105,6 +228,14 @@ fn get_input() -> HashMap<Cave, Vec<Cave>> {
 fn parse_cave(input: String) -> Cave {
     if input == input.to_uppercase() {
         return Cave::Large(input);
+    }
+
+    if input == "start" {
+        return Cave::Start;
+    }
+
+    if input == "end" {
+        return Cave::End;
     }
 
     Cave::Small(input)
