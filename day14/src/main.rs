364d8093477931b32072e5bc2d66b6b8b7b01b41
Mark Lister <mlister@scottlogic.com>
2021-12-14 15:29:17 +0000

    Don't use very long strings

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -2,64 +2,91 @@ use std::collections::HashMap;
 
 fn main() {
     println!("Day 14 part 1 {}", part1());
+    println!("Day 14 part 2 {}", part2());
 }
 
-fn part1() -> i32 {
+fn part1() -> i64 {
     let rules = get_input();
     let output = process_rules("VHCKBFOVCHHKOHBPNCKO".to_string(), rules, 10);
 
     let mut counts = HashMap::new();
 
-    for c in output.chars() {
-        *counts.entry(c).or_insert(0) += 1;
+    for t in output {
+        let chars = t.0.chars().collect::<Vec<_>>();
+
+        *counts.entry(chars[0]).or_insert(0) += t.1;
+        *counts.entry(chars[1]).or_insert(0) += t.1;
     }
 
     let max = counts.clone().into_iter().max_by_key(|&(_, count)| count);
     let min = counts.clone().into_iter().min_by_key(|&(_, count)| count);
 
-    max.unwrap().1 - min.unwrap().1
+    (max.unwrap().1 + 1) / 2 - (min.unwrap().1 + 1) / 2
 }
 
-fn process_rules(input: String, rules: HashMap<String, InsertionRule>, iterations: u32) -> String {
-    let mut current_string = input;
+fn part2() -> i64 {
+    let rules = get_input();
+    let output = process_rules("VHCKBFOVCHHKOHBPNCKO".to_string(), rules, 40);
+
+    let mut counts = HashMap::new();
+
+    for t in output {
+        let chars = t.0.chars().collect::<Vec<_>>();
+
+        *counts.entry(chars[0]).or_insert(0) += t.1;
+        *counts.entry(chars[1]).or_insert(0) += t.1;
+    }
+
+    let max = counts.clone().into_iter().max_by_key(|&(_, count)| count);
+    let min = counts.clone().into_iter().min_by_key(|&(_, count)| count);
+
+    (max.unwrap().1 + 1) / 2 - (min.unwrap().1 + 1) / 2
+}
+
+fn process_rules(
+    input: String,
+    rules: HashMap<String, InsertionRule>,
+    iterations: u32,
+) -> HashMap<String, i64> {
+    let mut hashmap = HashMap::new();
+
+    let chars = input.chars().collect::<Vec<_>>();
+
+    let windows = chars.windows(2);
+
+    for (_i, window) in windows.enumerate() {
+        let pair_string = window.iter().collect::<String>();
+
+        *hashmap.entry(pair_string).or_insert(0) += 1
+    }
+
     for _i in 0..iterations {
-        let chars = current_string.chars().collect::<Vec<_>>();
-        let mut new_chars = chars.clone();
-
-        let windows = chars.windows(2);
-
-        let mut new_chars_added = 0;
-
-        for (i, window) in windows.enumerate() {
-            let pair_string = window.iter().collect::<String>();
-
-            let insert_rule = rules.get(&pair_string);
+        let mut new_hashmap = HashMap::new();
+        for e in hashmap.iter() {
+            let insert_rule = rules.get(e.0);
 
             match insert_rule {
                 Some(x) => {
-                    // println!("rule: {:?} window: {}", x, pair_string);
+                    let output1 = &x.output.0;
+                    let output2 = &x.output.1;
 
-                    new_chars.splice(
-                        i + new_chars_added..=i + new_chars_added + 1,
-                        x.output.iter().cloned(),
-                    );
-                    new_chars_added += 1;
+                    *new_hashmap.entry(output1.clone()).or_insert(0) += e.1;
+                    *new_hashmap.entry(output2.clone()).or_insert(0) += e.1;
                 }
                 None => (),
             }
         }
 
-        current_string = new_chars.iter().collect::<String>();
-
-        println!("i {}", _i);
+        hashmap = new_hashmap;
     }
 
-    current_string
+    // println!("{:?}", hashmap);
+    hashmap
 }
 
 #[derive(Debug)]
 struct InsertionRule {
-    output: Vec<char>,
+    output: (String, String),
 }
 
 fn get_input() -> HashMap<String, InsertionRule> {
@@ -78,7 +105,10 @@ fn get_input() -> HashMap<String, InsertionRule> {
             (
                 split[0].to_string(),
                 InsertionRule {
-                    output: vec![pair.0, output, pair.1],
+                    output: (
+                        vec![pair.0, output].iter().collect::<String>(),
+                        vec![output, pair.1].iter().collect::<String>(),
+                    ),
                 },
             )
         })
